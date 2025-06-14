275c35059dc9f64d4ba461e016f372a41dcf027b
Rodrigo Hernandez <kwizatz@aeongames.com>
2025-06-13 20:42:47 -0600

    Added patching code to exe-font-patcher.

--- exe-font-patcher.cpp
+++ exe-font-patcher.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <cstring>
 #include <filesystem>
-#include <regex>
 #include <vector>
+#include <array>
+#include <tuple>
+
+std::array<std::pair<std::vector<uint8_t>, std::vector<uint8_t>>,2>
+replacers{{
+    {{0x6A,0x00,0x6A,0x00,0x6A,0x00,0x68,0x80,0x00,0x00,0x00,0x6A,0x00,0x6A,0x00},
+     {0x6A,0x00,0x6A,0x00,0x6A,0x00,0x6A,0x00,0x90,0x90,0x90,0x6A,0x00,0x6A,0x00}},
+    {{0x82,0x6C,0x82,0x72,0x20,0x96,0xBE,0x92,0xA9,0x00},
+     {'C', 'a', 'l', 'i', 'b', 'r', 'i', 0x00,0x00,0x00}}
+}};
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -21,10 +31,28 @@ int main(int argc, char* argv[]) {
     std::streampos fileSize = inputFile.tellg();
     inputFile.seekg(0, std::ios::beg);
     // Read the file contents into a vector
-    std::vector<uint8_t> buffer(fileSize);
+    std::string buffer{};
+    buffer.resize(fileSize);
     inputFile.read(reinterpret_cast<char*>(buffer.data()), fileSize);
     // Close the file
     inputFile.close();
+
+    size_t replacements{0};
+    for(size_t i=0; i<buffer.size(); ++i)
+    {
+        for(auto& j: replacers)
+        {
+            if(i+j.first.size()<buffer.size())
+            {
+                if(std::memcmp(buffer.data()+i,j.first.data(),j.first.size())==0)
+                {
+                    memcpy(buffer.data() + i,j.second.data(),j.second.size());
+                    ++replacements;
+                    i+=j.second.size();
+                }
+            }
+        }
+    }
 
     std::string output_file{};
     if (argc == 2) 
@@ -46,7 +74,7 @@ int main(int argc, char* argv[]) {
     outputFile.write(reinterpret_cast<char*>(buffer.data()),buffer.size());
     outputFile.close();
 
-    std::cout << "File patched successfully." << std::endl;
+    std::cout << "File patched successfully, " << replacements << " replacements." << std::endl;
 
     return 0;
 }

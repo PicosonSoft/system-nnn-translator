b292a96a90f5f023ef804beca78076078c4b823a
Rodrigo Hernandez <kwizatz@aeongames.com>
2025-06-28 20:25:50 -0600

    Don't format messages that do not need it.

--- formatter.cpp
+++ formatter.cpp
@@ -39,20 +39,44 @@ std::string ReplaceBadChars(const std::string& input)
     }
     return result;
 }
+std::vector<std::string> SplitLines(const std::string& str)
+{
+    std::regex regex("\r?\n");
+    std::vector<std::string> list(std::sregex_token_iterator(str.begin(), str.end(), regex, -1),
+                                  std::sregex_token_iterator());
+    return list;
+}
 
-std::string FormatLine(const std::string& str)
+std::string FormatLine(const std::string& str, size_t& not_formated, size_t& formated)
 {
     const static std::regex empty_space("[\\n\\r\\s]+");
     // We never modify the first line which is usually a character's name
     auto first_newline{str.find_first_of("\r\n")};
     if(first_newline==std::string::npos||first_newline+2==str.size())
     {
+        ++not_formated;
         return {first_newline==std::string::npos ? str + "\r\n" : str};
     }
-
     std::string first_line{ReplaceBadChars(str.substr(0,first_newline+2))};
     std::string second_line{ReplaceBadChars(str.substr(first_newline+2))};
-
+    {
+        auto lines = SplitLines(second_line);
+        bool format_needed{false};
+        for(auto& i: lines)
+        {
+            if(i.size() > static_cast<size_t>(48))
+            {
+                format_needed = true;
+                break;
+            }
+        }
+        if(!format_needed)
+        {
+            ++not_formated;
+            return str;
+        }
+    }
+    ++formated;
     second_line = std::regex_replace( second_line, empty_space, " " );
     // Remove leading whitespace
     second_line.erase(second_line.begin(), std::find_if(second_line.begin(), second_line.end(), [](unsigned char ch) {
@@ -95,6 +119,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    size_t not_formated{0};
+    size_t formated{0};
     std::string output_file{argc > 2  ? argv[2] : argv[1]};
     json input_json{};
     std::ifstream f(argv[1]);
@@ -120,7 +146,7 @@ int main(int argc, char* argv[]) {
 
     for(auto& i: input_json)
     {
-        output_json.push_back(json::object({{"text",FormatLine(i["text"])}}));
+        output_json.push_back(json::object({{"text",FormatLine(i["text"],not_formated,formated)}}));
     }
 
     std::cerr << "Writting " << output_file.c_str() << std::endl;
@@ -128,7 +154,7 @@ int main(int argc, char* argv[]) {
     o << std::setw(4) << output_json << std::endl;
     o.close();
 
-    std::cerr << "Formatting Complete" << std::endl;
+    std::cerr << "Formatting Complete, messages formated "<< formated<<" messages not formated " << not_formated << std::endl;
 
     return 0;
 }
